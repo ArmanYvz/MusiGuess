0eb57cd80f5f3a70f2039f6140b18a0ca6ddbbe8
Berat Soyakça <mberatsoyakca@gmail.com>
2022-03-30 22:14:15 +0300

    implement lobbies page

--- musiguess/src/App.js
+++ musiguess/src/App.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 import Login from "./components/Login/Login.js";
 import ResetPassword from "./components/ResetPassword/ResetPassword.js";
 import Register from "./components/Register/Register.js";
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
+import Lobbies from "./components/Lobbies/Lobbies";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route exact path = "/" element = {<Login/>} />
-          <Route exact path = "/resetpassword" element = {<ResetPassword/>} />
-          <Route exact path = "/register" element = {<Register/>} />
-          <Route exact path = "/home" element = {<Home/>} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/resetpassword" element={<ResetPassword />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="lobbies" element={<Lobbies />} />
         </Routes>
       </div>
     </Router>
-    
   );
 }
 
