bea9bf1423599ccd572c49c8eae2270e98594ad8
Berat Soyakça <mberatsoyakca@gmail.com>
2022-04-09 16:44:18 +0300

    lobby popup fix

--- musiguess/src/components/Lobbies/Lobbies.js
+++ musiguess/src/components/Lobbies/Lobbies.js
@@ -12,6 +12,8 @@ import { Button, Modal, Form } from 'react-bootstrap';
 
 import { db } from "../../firebase";
 
+import CreateLobbyPopup from "../CreateLobbyPopup/CreateLobbyPopup";
+
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -32,7 +34,8 @@ const Lobbies = () => {
   const [newLobbyName, setNewLobbyName] = useState("");
   const [newLobbyMaxPlayers, setNewLobbyMaxPlayers] = useState(5);
 
-  const handlePopupClose = () => setPopupShow(false);
+  const handlePopupClose = () => setPopupShow(false)
+  
   const handlePopupShow = () => setPopupShow(true);
 
   const handleBackHomeButton = () => {
@@ -68,9 +71,12 @@ const Lobbies = () => {
       roundEnded: "",
       wrongAnswers: [],
 
-
-
     });
+
+    setPopupShow(false);
+    setNewLobbyName("");
+    setNewLobbyMaxPlayers(5);
+
   }
 
   const handleJoinExistingLobby = () => {
@@ -97,78 +103,97 @@ const Lobbies = () => {
   }, []);
 
   return (
-    <div className="lobbies">
-      <div className="lobbiesHeader">
-        <div className="lobbiesHeader__logoText">
-          <p className="lobbiesHeader__logoText__top">Musi</p>
-          <p className="lobbiesHeader__logoText__bottom">Guess</p>
-        </div>
-        <h1>Multiplayer Lobby List</h1>
-        <button
-          className="lobbiesHeader__backToHomeButton"
-          onClick={handleBackHomeButton}
-        >
-          <KeyboardBackspaceIcon fontSize="large" />
-        </button>
-      </div>
-      <div className="lobbiesBody">
-        <div className="lobbiesBody__table">
-          <div className="lobbiesBody__table__header">
-            <p>Lobby Name</p>
-            <p>Id </p>
-            <p>Status</p>
-            <p>Players</p>
+    <>
+      {
+      popupShow &&
+        <CreateLobbyPopup
+          handlePopupCreateLobbyButton = {handlePopupCreateLobbyButton}
+          newLobbyName={newLobbyName}
+          handlePopupClose = {handlePopupClose}
+          setNewLobbyName = {setNewLobbyName}
+          newLobbyMaxPlayers = {newLobbyMaxPlayers}
+          setNewLobbyMaxPlayers = {setNewLobbyMaxPlayers}
+        />
+        }
+      <div className="lobbies" style={popupShow ? {"filter": "blur(16px)"} : {"filter":"none"}}>
+        <div className="lobbiesHeader">
+          <div className="lobbiesHeader__logoText">
+            <p className="lobbiesHeader__logoText__top">Musi</p>
+            <p className="lobbiesHeader__logoText__bottom">Guess</p>
           </div>
-          <div className="lobbiesBody__table__body">
-            {lobbies.map((lobby) => {
-              return(
-                <div className="lobbiesBody__table_body_row">
-                  <p>{lobby.name}</p>
-                  <p>{lobby.lobbyID}</p>
-                  <p>{lobby.status}</p>
-                  <p>{lobby.players.length}/{lobby.maxPlayers}</p>
-                </div>
-              )
-            })}
+          <h1>Multiplayer Lobby List</h1>
+          <button
+            className="lobbiesHeader__backToHomeButton"
+            onClick={handleBackHomeButton}
+          >
+            <KeyboardBackspaceIcon fontSize="large" />
+          </button>
+        </div>
+        <div className="lobbiesBody">
+          <div className="lobbiesBody__table">
+            <div className="lobbiesBody__table__header">
+              <p>Lobby Name</p>
+              <p>Id </p>
+              <p>Status</p>
+              <p>Players</p>
+            </div>
+            <div className="lobbiesBody__table__body">
+              {lobbies.map((lobby) => {
+                return(
+                  <div className="lobbiesBody__table_body_row">
+                    <p>{lobby.name}</p>
+                    <p>{lobby.lobbyID}</p>
+                    <p>{lobby.status}</p>
+                    <p>{lobby.players.length}/{lobby.maxPlayers}</p>
+                  </div>
+                )
+              })}
+              
+            </div>
+          </div>
+          <div className="lobbiesButtons">
+            <button className = "lobbiesBody__create_button" onClick= {handlePopupShow}>Create New Lobby</button>
+            <div className="lobbiesJoinButton">
+              <input type="text" className="joinLobbyTextbox" value ={joinLobbyId} onChange={(event) => {setJoinLobbyId(event.target.value);}} onInput={(event) => setJoinLobbyId(event.target.value)}></input>
+              <button disabled={joinLobbyId === ""} className = "lobbiesBody__join_button" onClick= {handleJoinExistingLobby}>Join Lobby with ID</button>
+            </div>
             
-          </div>
-        </div>
-        <div className="lobbiesButtons">
-          <button className = "lobbiesBody__create_button" onClick= {handlePopupShow}>Create New Lobby</button>
-          <div className="lobbiesJoinButton">
-            <input type="text" className="joinLobbyTextbox" value ={joinLobbyId} onChange={(event) => {setJoinLobbyId(event.target.value);}} onInput={(event) => setJoinLobbyId(event.target.value)}></input>
-            <button disabled={joinLobbyId === ""} className = "lobbiesBody__join_button" onClick= {handleJoinExistingLobby}>Join Lobby with ID</button>
           </div>
           
         </div>
+
         
-      </div>
 
-      <Modal show={popupShow} className = "modal" onHide={handlePopupClose}>
-        <Modal.Header closeButton>
-          <p></p>
-        </Modal.Header>
-        <Modal.Body className="modal-body">
-          <div className="popupNewLobbySettings">
-            <h3>Lobby Name:</h3>
-            <input type="text" className="newLobbyNameTextbox" value ={newLobbyName} onChange={(event) => {setNewLobbyName(event.target.value);}} onInput={(event) => setNewLobbyName(event.target.value)}></input>
-            <h3>Max # of Players: {newLobbyMaxPlayers}</h3>
-            <input type="range" class="form-range" id="newLobbyMaxPlayersRange" min="2" max="10" step="1" defaultValue={newLobbyMaxPlayers} onChange={ (event) => setNewLobbyMaxPlayers(event.target.value)} value={newLobbyMaxPlayers}></input>
-        
-          </div>
+        {/* <Modal show={popupShow} className = "modal" onHide={handlePopupClose}>
+          <Modal.Header closeButton>
+            <p></p>
+          </Modal.Header>
+          <Modal.Body className="modal-body">
+            <div className="popupNewLobbySettings">
+              <h3>Lobby Name:</h3>
+              <input type="text" className="newLobbyNameTextbox" value ={newLobbyName} onChange={(event) => {setNewLobbyName(event.target.value);}} onInput={(event) => setNewLobbyName(event.target.value)}></input>
+              <h3>Max # of Players: {newLobbyMaxPlayers}</h3>
+              <input type="range" class="form-range" id="newLobbyMaxPlayersRange" min="2" max="10" step="1" defaultValue={newLobbyMaxPlayers} onChange={ (event) => setNewLobbyMaxPlayers(event.target.value)} value={newLobbyMaxPlayers}></input>
+          
+            </div>
 
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={handlePopupClose}>
-              Cancel
-          </Button>
-          <Button variant="success" disabled={newLobbyName === ""} onClick={handlePopupCreateLobbyButton}>
-              Create New Lobby
-          </Button>
-        </Modal.Footer>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={handlePopupClose}>
+                Cancel
+            </Button>
+            <Button variant="success" disabled={newLobbyName === ""} onClick={handlePopupCreateLobbyButton}>
+                Create New Lobby
+            </Button>
+          </Modal.Footer>
 
-      </Modal>
+        </Modal> */}
     </div>
+
+
+
+    </>
+
   );
 };
 
